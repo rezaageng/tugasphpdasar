df87d32bae672ea5e3c45126106d5a251170c36f
Reza Ageng <rezaageng5@gmail.com>
2020-03-27 13:46:14 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+require 'functions.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas PWPB</title>
+    <link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body >
+    <div class="container" id="particles-js">
+        <div class="title">
+            <img src="img/hello.png" alt="Hello World">
+        </div>
+        <div class="button">
+            <ul>
+                <li><a href="">Percabangan</a></li>
+                <li><a href="">Pengulangan</a></li>
+                <li><a href="">Array</a></li>
+                <li><a href="">Function</a></li>
+                <li><a href="">Oprasi Aritmatika dan logika</a></li>
+            </ul>
+        </div>
+    </div>
+<script src="js/particles.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
